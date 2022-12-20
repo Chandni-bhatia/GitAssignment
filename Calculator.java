3824077cef402b19f402130d88b7b2d6627e09f0
Chandni <Chandni@nagarro.com>
2022-12-20 23:37:13 +0530

    Addition class is created and methods are added in the Addition branch

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,10 @@
 class Calculator{
-    
+
     public static void main(String[] args) {
-        
+        Addition a =new Addition();
+      int result1=  a.add(12, 12);
+      float result2= a.addFloat(15.2f, 12.9f);
+      System.out.println("Result1- "+result1);
+      System.out.println("Result2- "+result2);
     }
 }
